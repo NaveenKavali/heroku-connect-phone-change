0293b3fe42c459cd675b3663d572793ad4c6b4f7
NaveenKavali <kavalinaveennaidu@gmail.com>
2016-11-10 15:14:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ app.post('/update', function(req, res) {
         // watch for any connect issues
         if (err) console.log(err);
         conn.query(
-            'UPDATE salesforce.Contact SET Phone = $1, MobilePhone = $1 WHERE LOWER(FirstName) = LOWER($2) AND LOWER(LastName) = LOWER($3) AND LOWER(Email) = LOWER($4)',
-            [req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
+            'UPDATE salesforce.Case SET product_name__c = $1 WHERE LOWER(initial_patient_name__c) = LOWER($2) AND LOWER(initial_patient_surname__c) = LOWER($3) AND LOWER(age__c) = LOWER($4)',
+            [req.body.product_name__c.trim(), req.body.initial_patient_name__c.trim(), req.body.initial_patient_surname__c.trim(), req.body.age__c.trim()],
             function(err, result) {
                 if (err != null || result.rowCount == 0) {
-                  conn.query('INSERT INTO salesforce.Contact (Phone, MobilePhone, FirstName, LastName, Email) VALUES ($1, $2, $3, $4, $5)',
-                  [req.body.phone.trim(), req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
+                  conn.query('INSERT INTO salesforce.Case (product_name__c, initial_patient_name__c, initial_patient_surname__c, age__c) VALUES ($1, $2, $3, $4)',
+                  [req.body.product_name__c.trim(), req.body.initial_patient_name__c.trim(), req.body.initial_patient_surname__c.trim(), req.body.age__c.trim()],
                   function(err, result) {
                     done();
                     if (err) {
